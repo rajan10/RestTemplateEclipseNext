2c71ea9a31079b1ed412ca1ac8092f4d576e72c3
Rajan Gauchan <hirajan10@hotmail.com>
2024-01-21 02:34:17 -0500

    Add rest Client. RestTemplate is the core class for making HTTP requests in the Spring Framework, while RestTemplateBuilder is a builder class that helps in creating and customizing RestTemplate instances with additional configuration options. Using RestTemplateBuilder is often preferred when you need more control over the configuration of your REST client

--- src/main/java/com/app/client/BookClient.java
+++ src/main/java/com/app/client/BookClient.java
@@ -1,5 +1,6 @@
 package com.app.client;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,40 +13,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 //restclient business logic is writtern under service
 public class BookClient{
 	
+	private final RestTemplate restTemplate;
+	public BookClient(RestTemplateBuilder restTemplateBuilder)//RestTemplateBuilder is injected into the 'BookClient' 
+	{
+		this.restTemplate=restTemplateBuilder.build();//create a instance of RestTemplate and configure it using build()
+	}
+	
 	public void invokeBookTicket() {
-//		String apiurl="https://fakerestapi.azurewebsites.net/api/v1/Books";
-//		Book book = new Book();
-//		book.setId(1001);
-//		book.setTitle("Rajan rocks");
-//		book.setDescription("Autobiographical book");
-//		book.setPageCount(100);
-//		book.setExcerpt("This is about the excerpt here of the book Rajan rocks");
-//		book.setPublishDate("1869-01-20");
-		
-		String json ="{\"id\":1001,\"title\":\"Sajan rocks\",\"description\":\"Autobiographical book\",\"pageCount\":100,\"excerpt\":\"This is about the excerpt here of the book Sajan rocks\",\"publishDate\":\"1869-01-20\"}";
-		// Use Jackson library to parse the JSON String to Java object
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-			//Deserialize JSON to Java object
-			Book sajanRocks = mapper.readValue(json, Book.class);
-			//Access individual attributes
-			System.out.println(sajanRocks.getId());
-			System.out.println(sajanRocks.getTitle());
-			System.out.println(sajanRocks.getDescription());
-			System.out.println(sajanRocks.getPageCount());
-			System.out.println(sajanRocks.getExcerpt());
-			System.out.println(sajanRocks.getPublishDate());
-			
-		} 
-		catch (JsonProcessingException e) 
-		{
-			e.printStackTrace();
-			
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
+		String apiurl="https://fakerestapi.azurewebsites.net/api/v1/Books";
+		Book book = new Book();
+		book.setId(10010);
+		book.setTitle("Rajan RestTemplateBuilder rocks");
+		book.setDescription("Autobiographical book");
+		book.setPageCount(1000);
+		book.setExcerpt("This is about the excerpt here of the book Rajan RestTemplateBuilder rocks");
+		book.setPublishDate("2024-01-20");
+	  
+		ResponseEntity<String> postForEntity =restTemplate.postForEntity(apiurl, book, String.class);
+		System.out.println(postForEntity.getBody());
 	}
 }
 			
